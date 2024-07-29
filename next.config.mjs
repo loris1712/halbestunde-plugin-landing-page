a2e55b70c2df8ec0aee36757e5816a7bc9e1255e
Loris Caputo <46825967+loris1712@users.noreply.github.com>
2024-07-29 12:22:14 +0200

    Changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,32 +2,7 @@
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack(config) {
-    // Aggiungi supporto per i file SVG
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            svgoConfig: {
-              plugins: [
-                {
-                  name: 'removeViewBox',
-                  active: false,
-                },
-              ],
-            },
-          },
-        },
-      ],
-    });
-
-    // Altre configurazioni Webpack possono essere aggiunte qui
-
-    return config;
-  },
+  reactStrictMode: false,
   images: {
     domains: ['example.com'], // Aggiungi i domini delle immagini
   },
